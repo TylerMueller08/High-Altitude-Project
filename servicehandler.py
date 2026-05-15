82eb11a0c7c0763ed44141ef49601f7fea95c0f1
TylerMueller08 <TylerJacobMueller08@gmail.com>
2026-05-15 14:29:15 -0600

    Fixed typo

--- servicehandler.py
+++ servicehandler.py
@@ -4,7 +4,7 @@ from serviceworker import ServiceWorker
 
 class ServiceHandler:
     def __init__(self):
-        self.i2c = board.I2C(board.SCL, board.SDA)
+        self.i2c = busio.I2C(board.SCL, board.SDA)
         self.bme280 = adafruit_bme280.Adafruit_BME280_I2C(self.i2c)
         self.bme280.sea_level_pressure = 1013.25
 
