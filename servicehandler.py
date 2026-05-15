7999376354133cf07319504cd10db2a384fa2837
TylerMueller08 <TylerJacobMueller08@gmail.com>
2026-05-15 14:28:37 -0600

    Updated to force Bus 3

--- servicehandler.py
+++ servicehandler.py
@@ -1,10 +1,10 @@
-import board
+import board, busio
 from adafruit_bme280 import basic as adafruit_bme280
 from serviceworker import ServiceWorker
 
 class ServiceHandler:
     def __init__(self):
-        self.i2c = board.I2C()
+        self.i2c = board.I2C(board.SCL, board.SDA)
         self.bme280 = adafruit_bme280.Adafruit_BME280_I2C(self.i2c)
         self.bme280.sea_level_pressure = 1013.25
 
